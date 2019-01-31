79305147e0ce94e3eec778b1ca64780571d7ed76
devmichalek <devmichalek@gmail.com>
2019-02-01 00:39:14 +0100

    added x, y for wall

--- lib/wall.pm
+++ lib/wall.pm
@@ -5,8 +5,32 @@ sub new {
     my ($class, %args) = @_;
     my $self = bless {}, $class;
 
+    $self->setX($args{x});
+    $self->setY($args{y});
     $self->setType($args{type});
     return $self;
+}
+
+sub setX() {
+    my ($self, $value) = @_;
+    $self->{x} = $value;
+    return;
+}
+
+sub setY() {
+    my ($self, $value) = @_;
+    $self->{y} = $value;
+    return;
+}
+
+sub getY() {
+    my ($self) = @_;
+    return $self->{y};
+}
+
+sub getX() {
+    my ($self) = @_;
+    return $self->{x};
 }
 
 sub setType() {
