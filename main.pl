388b012bb0685fa58cd749745121a2ebee19de36
devmichalek <devmichalek@gmail.com>
2019-02-01 12:29:29 +0100

    Added comments, fixed bugs, code refactoring.

--- main.pl
+++ main.pl
@@ -1,4 +1,4 @@
-# include
+#Includes.
 use strict;
 use warnings;
 use GD::Simple;
@@ -7,160 +7,142 @@ use lib "$FindBin::Bin/lib";
 use Point;
 use Wall;
 
-# resolution
+#Resolution and settings
 my $EDGE_LONG = 20;
-
-# prepare image
-my $IMAGE_WIDTH = ($EDGE_LONG * 21) + 1;
+my $EDGE_MULTIPLIER = 21;
+my $IMAGE_WIDTH = ($EDGE_LONG * $EDGE_MULTIPLIER) + 1;
 my $IMAGE_HEIGHT = $IMAGE_WIDTH;
-my $img = GD::Simple->new($IMAGE_WIDTH, $IMAGE_HEIGHT);
-$img->fgcolor('black');
 
-# generate grid
-my @cells;		# cells with unique ids
-my @edges;
-my @vmatrix;	# vertical walls
-my @hmatrix;	# horizontal walls
-my $idc = 0;	# ID counter
-for (my $i = 0; $i < $IMAGE_WIDTH /  $EDGE_LONG; ++$i)
-{
-	for (my $j = 0; $j < $IMAGE_HEIGHT /  $EDGE_LONG; ++$j)
-	{
+#Generate grid.
+my @cells;		#Cells with unique ids.
+my @edges;		#Not used walls.
+my @vmatrix;	#Vertical walls.
+my @hmatrix;	#Horizontal walls.
+my $idc = 0;	#Counter of id.
+for (my $i = 0; $i < $IMAGE_WIDTH /  $EDGE_LONG; ++$i) {
+	for (my $j = 0; $j < $IMAGE_HEIGHT /  $EDGE_LONG; ++$j) {
 		$vmatrix[$i][$j] = Point->new(x => $i * $EDGE_LONG, y => $j * $EDGE_LONG);
 		$hmatrix[$i][$j] = Point->new(x => $i * $EDGE_LONG, y => $j * $EDGE_LONG);
-		$edges[$i][$j][0] = Wall->new(x => $i, y => $j, type => 0);
-		$edges[$i][$j][1] = Wall->new(x => $i, y => $j, type => 1);
-		$cells[$i][$j] = $idc++;
+		$edges[$i][$j][0] = Wall->new(x => $i, y => $j, type => 0); #0 represents the vertical wall.
+		$edges[$i][$j][1] = Wall->new(x => $i, y => $j, type => 1); #1 represents the hotizontal wall.
+		$cells[$i][$j] = $idc++; #Ascribe id to the cell.
 	}
 }
 
-
-
-# remove walls
-{
-	while (1)
-	{
-		# check how many ids are there
-		my $idxl = scalar(@cells);
-		my $idyl = scalar(@{$cells[0]});
-		my $isOneID = 1;
-		for (my $i = 0; $i < $idxl; ++$i) {
-			for (my $j = 0; $j < $idyl; ++$j) {
-				if ($cells[$i][$j] != $cells[0][0]) {
-					$isOneID = 0;
-					#print("There is still more than one ID\n");
-					last; # break
-				}
-			}
-		}
-		if ($isOneID) {
-			last; # there is only one ID
-		}
-
-		while (1) {
-			my $ret = 1; # ret code
-
-			# rand x
-			my $xl = scalar(@edges);
-			my $i = int(rand($xl));
-
-			if ($xl == 2) {
-				$isOneID = 1;
+#Remove walls.
+while (1) {
+	#Check how many ids are there.
+	#If more than one, continue.
+	my $idxl = scalar(@cells);
+	my $idyl = scalar(@{$cells[0]});
+	my $closeLoop = 1;
+	for (my $i = 0; $i < $idxl; ++$i) {
+		for (my $j = 0; $j < $idyl; ++$j) {
+			if ($cells[$i][$j] != $cells[0][0]) {
+				$closeLoop = 0;
 				last;
 			}
+		}
+	}
 
-			# rand y
-			my $yl = scalar(@{$edges[$i]});
-			my $j = int(rand($yl));
+	while (!$closeLoop) {
+		my $closeInnerLoop = 1;
 
-			# debug
-			#print("Scope(x) min=1 max=$xl\n");
-			#print("Scope(y) min=1 max=$yl\n");
-			#print("Rand \$x=$i \$y=$j\n");
+		#Rand x.
+		my $xl = scalar(@edges);
+		my $i = int(rand($xl));
+		#If there are only 2 rows (top and bot).
+		if ($xl == 2) {
+			$closeLoop = 1;
+			last;
+		}
 
-			# rand type
-			my $tl = 2;
-			my $it = 0;
-			if ($edges[$i][$j][0]->getType() == -1) {
-				$it = 1;
-				$tl = 1;
-			}
-			if ($edges[$i][$j][1]->getType() == -1) {
-				$it = 0;
-				$tl = 1;
-			}
-			
-			if ($tl == 2) {
-				$it = int(rand($tl));
-			}
+		#Rand y.
+		my $yl = scalar(@{$edges[$i]});
+		my $j = int(rand($yl));
 
-			#print("Choosing type from \$i=$i \$j=$j $tl\n");
-			my $t = $edges[$i][$j][$it]->getType(); # 0 - vertical / 1 - horizontal wall
-			#print("Type set to \$t=$t\n");
+		#Rand type.
+		my $tl = 2;
+		my $it = 0;
+		if ($edges[$i][$j][0]->getType() == -1) {
+			$it = 1;
+			$tl = 1;
+		}
+		if ($edges[$i][$j][1]->getType() == -1) {
+			$it = 0;
+			$tl = 1;
+		}
+		if ($tl == 2) {
+			$it = int(rand($tl)); #Rand which one.
+		}
+		my $t = $edges[$i][$j][$it]->getType(); # 0 - vertical / 1 - horizontal wall
 
-			
-			my $bx = $edges[$i][$j][$it]->getX();
-			my $by = $edges[$i][$j][$it]->getY();
-			my $newID = $cells[$bx][$by];
-			my $oldID = -1;
-			if ($t == 0) {
-				# $by stays the same, and <- $bx -> check cell(left, right)
-				if ($bx == 0 || $bx == ($idxl-1) || ($cells[$bx-1][$by] == $cells[$bx][$by])) {
-					$ret = 0; # we have the same id in these cells, repeat loop
-				} else {
-					$oldID = $cells[$bx-1][$by];
-					#print("Removing wall(v) \$bx=$bx \$by=$by\n");
-					$vmatrix[$bx][$by] = Point->new(x => -1, y => -1);
-				}
+		#Set proper x and y, new id and old id.
+		my $bx = $edges[$i][$j][$it]->getX();
+		my $by = $edges[$i][$j][$it]->getY();
+		my $newID = $cells[$bx][$by];
+		my $oldID = -1;
+
+		if ($t == 0) {
+			# $by stays the same, and <- $bx -> check cell(left, right)
+			if ($bx == 0 || $bx == ($idxl-1) || ($cells[$bx-1][$by] == $cells[$bx][$by])) {
+				$closeInnerLoop = 0; #We have the same id in these cells, repeat loop.
 			} else {
-				# $bx stays the same, and <- $by -> check cell(top, bot)
-				if ($by == 0 || $by == ($idyl-1) || ($cells[$bx][$by-1] == $cells[$bx][$by])) {
-					$ret = 0; # we have the same id in these cells, repeat loop
-				} else {
-					$oldID = $cells[$bx][$by-1];
-					#print("Removing wall(h) \$bx=$bx \$by=$by\n");
-					$hmatrix[$bx][$by] = Point->new(x => -1, y => -1);
-				}
+				$oldID = $cells[$bx-1][$by];
+				#print("Removing wall(v) \$bx=$bx \$by=$by\n");
+				$vmatrix[$bx][$by] = Point->new(x => -1, y => -1);
 			}
-
-			if ($tl == 1) {
-				if ($yl == 2) {
-					#print("Deleting whole column \$i=$i\n");
-					#sleep(1);
-					splice(@edges, $i, 1); # delete whole column
-				} else {
-					#print("Deleting last wall \$i=$i \$j=$j\n");
-					#sleep(1);
-					splice(@{$edges[$i]}, $j, 1); # last wall
-				}
+		} else {
+			# $bx stays the same, and <- $by -> check cell(top, bot)
+			if ($by == 0 || $by == ($idyl-1) || ($cells[$bx][$by-1] == $cells[$bx][$by])) {
+				$closeInnerLoop = 0; #We have the same id in these cells, repeat loop.
 			} else {
-					#print("edges[$i][$j][$t] = $edges[$i][$j][$t]\n");
-					#sleep(1);
-					$edges[$i][$j][$t]->setType(-1); # first wall
+				$oldID = $cells[$bx][$by-1];
+				$hmatrix[$bx][$by] = Point->new(x => -1, y => -1);
 			}
+		}
 
-			if ($ret == 1) {
-				# set new ID to old IDs
-				for (my $k = 0; $k < $idxl; ++$k) {
-					for (my $l = 0; $l < $idyl; ++$l) {
-						if ($cells[$k][$l] == $oldID) {
-							$cells[$k][$l] = $newID;
-						}
+		#There is only one type of wall (v/h).
+		if ($tl == 1) {
+			#Delete whole column.
+			if ($yl == 2) {
+				splice(@edges, $i, 1);
+			}
+			#Delete last wall.
+			else {
+				splice(@{$edges[$i]}, $j, 1);
+			}
+		}
+
+		#There are two walls (v and h).
+		else {
+			$edges[$i][$j][$t]->setType(-1); 
+		}
+
+		if ($closeInnerLoop) {
+			# Set new id to old ids.
+			for (my $k = 0; $k < $idxl; ++$k) {
+				for (my $l = 0; $l < $idyl; ++$l) {
+					if ($cells[$k][$l] == $oldID) {
+						$cells[$k][$l] = $newID;
 					}
 				}
-
-				last; # break
 			}
+			last; #Break inner loop.
 		}
+	}
 
-		if ($isOneID) {
-			last; # there is only one ID
-		}
+	if ($closeLoop) {
+		last;
 	}
 }
 
-# print walls into image
+
+# Draw walls in image.
+my $img = GD::Simple->new($IMAGE_WIDTH, $IMAGE_HEIGHT);
+$img->fgcolor('black');
 my $size = scalar(@vmatrix);
+
 for (my $i = 0; $i < $size; ++$i)
 {
 	my $sv = scalar(@{$vmatrix[$i]});
@@ -180,7 +162,7 @@ for (my $i = 0; $i < $size; ++$i)
 	}
 }
 
-# draw start, end
+# Draw start and end.
 $img->fgcolor('white');
 $img->bgcolor('red');
 $img->rectangle(2, 2, $EDGE_LONG - 2, $EDGE_LONG - 2);
@@ -188,8 +170,7 @@ my $corner = $IMAGE_WIDTH - $EDGE_LONG;
 $img->bgcolor('green');
 $img->rectangle($corner + 2, $corner + 2, $corner + $EDGE_LONG - 2, $corner + $EDGE_LONG - 2);
 	
-
-# convert into png data
+# Convert into png data.
 open my $out, '>', 'maze.png' or die;
 binmode $out;
 print $out $img->png;
